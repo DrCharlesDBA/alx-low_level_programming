942d9c40c50cd22c3fcb16002d6bfb0fcca24348
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-19 21:57:48 -0800

    2-mul.c task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Return the output of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
