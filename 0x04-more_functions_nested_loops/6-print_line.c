831546ae1014316adde8b930c0e5551ad7d6f9d8
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-19 23:37:33 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @l: number of times
+ * Return: void
+ */
+
+void print_line(int l)
+{
+	int a;
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+}
