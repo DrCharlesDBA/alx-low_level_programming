613b1fe07a37fd6ca6eeb3ac4ad33a342bef4ae0
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-20 06:07:04 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - the value of n is restarted to become 98
+ * 
+ * @n: a new number is returned
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
