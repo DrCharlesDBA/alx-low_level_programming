fb45194c5624eff04bcb7841b6d62023deb6d5bb
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-20 00:00:48 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @a: parameter
+ * Return: returns void
+ */
+
+void print_diagonal(int a)
+{
+	int len, space;
+
+	if (a > 0)
+	{
+		for (len = 0; len < a; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				putchar(' ');
+			}
+
+			putchar('\\');
+
+			if (len == (a - 1))
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
