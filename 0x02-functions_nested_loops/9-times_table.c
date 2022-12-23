d9d775f08281d94a40932018a4d7a232797bdff0
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-23 15:54:34 -0800

    update white space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,42 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
+ * times_table - prints the 9 times table
+ * Description: this prints 9 timesthe table
+ * Return: this returns void
  */
 
-int main(void)
+void times_table(void)
 {
-	times_table();
-	return (0);
+	int row, column, product, tens, ones;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			tens = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
