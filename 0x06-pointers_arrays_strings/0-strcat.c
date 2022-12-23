f8bc243cce865b0d0b791f03a26b63cbe5ddce29
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-23 01:12:50 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, x = 0;
+	int i, j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (dest[x] != '\0')
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
-		dest[i] =src[x];
-		x++;
-		i++;
+		dest[i] = src[j];
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
