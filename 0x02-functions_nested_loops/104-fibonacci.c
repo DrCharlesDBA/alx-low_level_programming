0500c150f27bdd716c276bfd2a7f7c10e5aeb1d6
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-28 06:37:34 -0800

    16 -  program to print the first 100 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,20 +6,37 @@
 
 int main(void)
 {
-	unsigned long int x = 1, y = 2;
-	unsigned long int n;
+	unsigned long int a;
+	unsigned long int before = 1;
+	unsigned long int after = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int before1;
+	unsigned long int before2;
+	unsigned long int after1;
+	unsigned long int after2;
 
-	for (n = 0; n < 49; n++)
+	printf("%lu", before);
+
+	for (a = 1; a < 91; a++)
 	{
-		printf("%lu, %lu", x, y);
-		if (n != 48)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		x = x + y;
-		y = x + y;
+		printf(", %lu", after);
+		after += before;
+		before = after - before;
 	}
-	putchar('\n');
+	before1 = (before / l);
+	before2 = (before % l);
+	after1 = (after / l);
+	after2 = (after % l);
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", after1 + (after2 / l));
+		printf("%lu", after2 % l);
+		after1 = after1 + before1;
+		before1 = after1 - before1;
+		after2 = after2 + before2;
+		before2 = after2 - before2;
+	}
+	printf("\n");
 	return (0);
 }
