bd8fc8682821b475f270f03ca055b322ae762755
DrCharlesDBA <drcharlesdba@gmail.com>
2022-12-30 00:21:13 -0800

    11. The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,20 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 	int hundreds = '0';
+
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		for(tens = '0'; tens <= '9'; tens++)
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for(ones = '0'; ones <= '9'; ones++)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if(!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens))) /*eliminates repitition */
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens))) /*removesrepitition */
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && 
-								tens == '8')) /* adds commas and spaces*/
+					if (!(ones == '9' && hundreds == '7' &&	tens == '8')) /* addscommaspaces*/
 					{
 						putchar(',');
 						putchar(' ');
@@ -31,7 +31,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
-		
-
